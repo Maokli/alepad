4c32eaafbedf5602c426c5baccd671e489d0487f
Maokli2X <louayinfo07@gmail.com>
2021-07-31 00:39:28 +0100

    tests

--- ControllerTests/MessagesTest.cs
+++ ControllerTests/MessagesTest.cs
@@ -6,6 +6,8 @@ using API.Interfaces;
 using API.Models;
 using API.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 using Moq;
 using Xunit;
 
@@ -19,10 +21,22 @@ namespace ControllerTests
             //Arrange
             var mockMessageRepo = new Mock<IMessageRepository>();
 
+
             mockMessageRepo.Setup(repo=> repo.SaveAllAsync())
                 .ReturnsAsync(true);
-            var controller = 
-                new MessageController(mockMessageRepo.Object);
+
+            var fakeUserPrincipal = 
+                new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("userName",""),
+                    new Claim("userId","0"),
+                }, "mock"));
+            
+            var controller = new MessageController(mockMessageRepo.Object);
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = fakeUserPrincipal }
+            };
             
             //Act
             var result = await controller.AddMessage(new CreateMessageDto());
@@ -36,17 +50,43 @@ namespace ControllerTests
         {
             //Arrange
             var mockMessageRepo = new Mock<IMessageRepository>();
-
+            
             mockMessageRepo.Setup(repo => repo.SaveAllAsync())
                 .ReturnsAsync(false);
+
+            var fakeUserPrincipal = 
+                new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("userName",""),
+                    new Claim("userId","0"),
+                }, "mock"));
             
             var controller = new MessageController(mockMessageRepo.Object);
-            
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = fakeUserPrincipal }
+            };
+
             //Act
             var result = await controller.AddMessage(new CreateMessageDto());
 
             //Assert
             Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task AddMessage_ReturnsUnauthorized()
+        {
+            //Arrange
+            var mockMessageRepo = new Mock<IMessageRepository>();
+            
+            var controller = new MessageController(mockMessageRepo.Object);
+
+            //Act
+            var result = await controller.AddMessage(new CreateMessageDto());
+
+            //Assert
+            Assert.IsType<UnauthorizedResult>(result);
         }
 
         [Fact]
